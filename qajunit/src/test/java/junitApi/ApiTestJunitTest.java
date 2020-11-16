2e7ebd7a3bffe77a9a437aa6964e84c49f35fdd3
secnow1 <nowakowski.m.dawid@gmail.com>
2020-11-16 15:18:11 +0100

    Junit first tests

---
+++ qajunit/src/test/java/junitApi/ApiTestJunitTest.java
@@ -0,0 +1,15 @@
+package junitApi;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DisplayName("Api test junit")
+public class ApiTestJunitTest {
+
+    @Test
+    public void sampleApiTEst() {
+        assertTrue(true);
+    }
+}
